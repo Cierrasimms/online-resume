f364b501e7447fbb8241e5f9e464f1b037e2b0a6
CierraSimms <cierrasimms98@gmail.com>
2022-05-02 22:29:03 -0400

    started API request in ResumeIndex.vue

--- src/views/ResumeIndex.vue
+++ src/views/ResumeIndex.vue
@@ -1,12 +1,25 @@
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      people: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexResume();
+  },
+  methods: {
+    indexResume: function () {
+      axios.get("https://swapi.dev/api/people/").then((response) => {
+        // console.log(response.data.results);
+        this.people = response.data.results;
+        console.log(this.people);
+      });
+    },
+  },
 };
 </script>
 
