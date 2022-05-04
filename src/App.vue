1ecf601489c6d595e8a77aaac161734990387bb6
mattjkatz <matthewkatz7@gmail.com>
2022-05-03 21:33:34 -0500

    begun wokring on a header and messed with fonts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-    |
-    <router-link to="/resumes">Resumes</router-link>
-    |
+  <!-- Image and text -->
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img src="./assets/super-original-logo.png" width="60" height="45" class="d-inline-block align-top" alt="" />
+      Resumes-R-Us
+    </a>
     <router-link to="/my-resume/:id">My Resume</router-link>
+    <router-link to="/resumes">Resumes</router-link>
   </nav>
   <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Josefin Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
