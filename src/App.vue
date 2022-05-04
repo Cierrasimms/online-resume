772ca2f0c904669a6f4ac335d02ca83f9fe458a9
CierraSimms <cierrasimms98@gmail.com>
2022-05-04 17:18:45 -0400

    removed headers

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-    |
-    <router-link to="/resumes">Resumes</router-link>
-    |
     <router-link to="/my-resume/:id">My Resume</router-link>
   </nav>
   <router-view />
