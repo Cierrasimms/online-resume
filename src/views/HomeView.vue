557816f0ef1952cd679aded7ed1c567933c55de8
mattjkatz <matthewkatz7@gmail.com>
2022-05-03 20:58:00 -0500

    changed some stuff with the theme we were trying to use

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,19 @@ export default {
 </script>
 
 <template>
+  <!-- Image and text -->
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img
+        src="/docs/4.0/assets/brand/bootstrap-solid.svg"
+        width="30"
+        height="30"
+        class="d-inline-block align-top"
+        alt=""
+      />
+      Bootstrap
+    </a>
+  </nav>
   <div class="home">
     <h1>{{ message }}</h1>
   </div>
